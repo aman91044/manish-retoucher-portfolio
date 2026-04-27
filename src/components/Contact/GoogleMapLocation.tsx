cc64ccb7b7041b803dfe35d6e36c9aeb887b90d6
aman91044 <amanchauhan91044@gmail.com>
2026-04-27 16:18:00 +0530

    Updated  Google Maps Location .tsx

--- src/components/Contact/GoogleMapLocation.tsx
+++ src/components/Contact/GoogleMapLocation.tsx
@@ -10,7 +10,7 @@ const GoogleMapLocation = (): JSX.Element => {
       <div className="w-full max-w-6xl aspect-video rounded-lg overflow-hidden shadow-lg">
         <iframe
           title="Google Map showing location in..."
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13268.081468727249!2d-84.39893880749905!3d33.760139552551536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5045d6993098d%3A0x66fede2f990b630b!2sAtlanta%2C%20Georgia%2C%20EE.%20UU.!5e0!3m2!1ses!2sar!4v1745281810351!5m2!1ses!2sar"
+          src="https://maps.app.goo.gl/SrhM4E1MQGUroUxT9"
           className="w-full h-full"
           style={{border: "0"}}
           allowFullScreen
